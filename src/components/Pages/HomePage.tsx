f5ca8753da4d49e9ace814a57a23b88458c32eab
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-06-25 20:57:21 +0100

    Added dropdown

--- src/components/Pages/HomePage.tsx
+++ src/components/Pages/HomePage.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import { RouteComponentProps } from '@reach/router'
 import usePriceInput from '../Hooks/usePriceInput'
+import useDropDown from '../Hooks/useDropDown'
 
 const HomePage: React.FunctionComponent<RouteComponentProps> = () => {
 
 const [price, PriceInput, setPrice] = usePriceInput('Price', 'enter a price')
-
+const [selected, DropDown, setSelected] = useDropDown("Cars", "", ["One", "Two", "Three"])
   
   function submitFormData(e: React.FormEvent<HTMLFormElement>) {}
   return (
@@ -13,6 +14,9 @@ const [price, PriceInput, setPrice] = usePriceInput('Price', 'enter a price')
       <form onSubmit={submitFormData}>
         <div>
           <PriceInput />
+        </div>
+        <div>
+          <DropDown />
         </div>
       </form>
     </div>
