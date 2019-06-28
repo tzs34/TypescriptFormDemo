653f68dc6f53fde61b454e9553f34bdec12ed8a3
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-06-28 17:36:44 +0100

    Added an index.ts file for hook imports

---
+++ src/components/Hooks/index.ts
@@ -0,0 +1,5 @@
+import usePriceInput from './usePriceInput'
+import useDropDown from './useDropDown'
+import useRadioGroup from './useRadioGroup'
+
+export { useDropDown, usePriceInput, useRadioGroup }
