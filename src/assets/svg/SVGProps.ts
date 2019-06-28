f3f887d0d53a6ea751bd9a0ef189cf45bb9e9ea5
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-06-28 17:35:53 +0100

    Added required svgs

---
+++ src/assets/svg/SVGProps.ts
@@ -0,0 +1,4 @@
+export default interface SVGProps {
+  width?: number
+  height?: number
+}
--- src/assets/svg/error.tsx
+++ src/assets/svg/error.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import SVGProps from './SVGProps'
 
-const Error  = ({ width = 40, height = 40 }: SVGProps) => (
+const Error = ({ width = 40, height = 40 }: SVGProps) => (
   <svg width={width} height={height} role="img" aria-labelledby="error-svg">
     <title id="error-svg">Zonal logo</title>
-  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-  <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+    <path d="M0 0h24v24H0z" fill="none" />
   </svg>
 )
 
