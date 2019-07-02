bc378fa462e04be4a5179d6390c86dac4fb76449
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-07-02 16:50:15 +0100

    Removed interface and imported from interface file. Added onVALDATE CALLBACK

--- src/components/Common/FormValidator.tsx
+++ src/components/Common/FormValidator.tsx
@@ -1,27 +1,37 @@
 import * as React from 'react'
 import { ErrorSuccessIndicator } from '../../components'
 
-interface validatioProps {
+interface validatedProps {
   id: string
   isValid: boolean
 }
+
 interface FormValidatorProps {
+  children: React.ReactChild
+  id: string
   value: string | number
   validationFunction: (value: string | number) => boolean
+  onValidate?: (obj: validatedProps) => void
   validate?: boolean
 }
 
 const FormValidator: React.FunctionComponent<FormValidatorProps> = ({
   children,
+  id,
   value,
   validationFunction,
+  onValidate,
   validate = false
 }) => {
   const [valid, setValid] = React.useState(false)
 
   React.useEffect(() => {
     if (validate) {
-      let isValid = setValid(validationFunction(value))
+      let isValid = validationFunction(value)
+      setValid(isValid)
+      if (onValidate) {
+        onValidate({ id, isValid })
+      }
     }
   })
 
