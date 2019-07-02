a4110aceac3c2068243f73d8d6854fffdbaa8a7d
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-07-02 16:49:11 +0100

    Added depositcost component to index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export {
   default as ErrorSuccessIndicator
 } from './Common/ErrorSuccessIndicator'
 export { default as FormValidator } from './Common/FormValidator'
+export { default as DepositCostField } from './Common/DepositCostField'
