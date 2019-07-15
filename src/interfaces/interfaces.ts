7ba9e856380146a3db97a05f3246b14adaf10e1b
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-07-15 17:21:52 +0100

    Added interfaces for mock data

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -2,3 +2,41 @@ export interface validationProps {
   id: string
   isValid: boolean
 }
+
+interface titleProps {
+  name: string
+  variant: string
+}
+
+interface pricingProps {
+  cashPricePrefix: string
+  cashPrice?: number
+  monthlyPayment?: number
+  deposit?: number
+  financeHeading?: string
+}
+
+interface salesInfoProps {
+  pricing: pricingProps
+  summary: object
+  highlightedFeature: string
+}
+
+interface searchResultsProps {
+  salesInfo: salesInfoProps
+  isReserved: boolean
+  make: string
+  model: string
+  title: titleProps
+}
+
+export interface carDataProps {
+  availableMakes: string[][]
+  count: number
+  delivery: object
+  banner: object
+  searchResults: searchResultsProps[]
+  searchTitle: string
+  title: string
+  pagination: object
+}
