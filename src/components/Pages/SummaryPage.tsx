615a5b92908b33c00b59d4998549260dd37b2b26
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-07-15 17:21:30 +0100

    Added process mock data and carList

--- src/components/Pages/SummaryPage.tsx
+++ src/components/Pages/SummaryPage.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react'
-import { RouteComponentProps } from '@reach/router'
-
+import { RouteComponentProps, Link } from '@reach/router'
+import mockData from '../../utils/mockData'
+import { processCarData, appConstants } from '../../utils/app-utils'
+import { CarList } from '../../components'
 const SummaryPage: React.FunctionComponent<
   RouteComponentProps<{ id: string }>
 > = ({ location }) => {
-  console.log(location.state)
-  return <div>Summary</div>
+  const [carData, setCarData] = React.useState([])
+  React.useEffect(() => {
+    // here would useFetch hook to get data from the api etc
+    // But as we should not do that - we can use mock data
+
+    let data = processCarData(mockData, location.state.price)
+    const { displayNumberCars } = appConstants
+    if (data.length < displayNumberCars) {
+      setCarData(data)
+    } else {
+      setCarData(data.slice(0, displayNumberCars))
+    }
+  }, [])
+
+  return (
+    <div>
+      <CarList data={carData} />
+    </div>
+  )
 }
 
 export default SummaryPage
