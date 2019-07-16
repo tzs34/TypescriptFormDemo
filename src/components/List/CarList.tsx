e78cfb0fa119af9be1a493dbad27c5c1c4d5f689
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-07-16 18:43:22 +0100

    Refactored car list to use a ist

--- src/components/List/CarList.tsx
+++ src/components/List/CarList.tsx
@@ -6,21 +6,36 @@ interface dataProps {
   price: string
 }
 interface ListProps {
-  data: any[]
-  onClick?: (e: React.MouseEvent) => void
+  data: dataProps[]
+  onClick: (price: string) => void
+  printPDF?: (price: string) => void
 }
-const CarList: React.FunctionComponent<ListProps> = ({ data, onClick }) => (
-  <li>
-    {data.map(({ make, description, price }) => {
-      return (
-        <div className="car-list-container">
-          <div>make</div>
-          <div>description</div>
-          <div>price</div>
+const CarList: React.FunctionComponent<ListProps> = ({
+  data,
+  onClick,
+  printPDF = null
+}) => {
+  return (
+    <li>
+      {data.map(({ make, description, price }, index) => (
+        <div className="car-display-container" key={`${index}${description}`}>
+          <div>
+            <div>{`Car Make:  ${make}`}</div>
+            <div>{`Car Details:  ${description}`}</div>
+            <div>{`Car Price:  ${price}`}</div>
+          </div>
+          <div>
+            <button onClick={() => onClick(price)}>
+              {'Loan Payment Summary'}
+            </button>
+            {printPDF && (
+              <button onClick={() => printPDF(price)}>{'Print PDF'}</button>
+            )}
+          </div>
         </div>
-      )
-    })}
-  </li>
-)
+      ))}
+    </li>
+  )
+}
 
 export default CarList
