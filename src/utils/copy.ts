ed45b5a63bad6f219cc9123ec1071dc035f6540d
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-07-19 17:30:26 +0100

    Added year options to copy.js

---
+++ src/utils/copy.ts
@@ -0,0 +1,18 @@
+export default {
+    headers:{
+        loadHeader: 'Load Details Form'
+    },
+    labels:{
+        priceLabel: 'Car price',
+        depositLabel: 'Deposit Amount',
+        deliveryLabel: 'Delivery date',
+        periodLabel: 'Loan Period (Years)',
+        submitLabel: 'Submit'
+    },
+    placeholders:{
+        carPlaceholder: 'Enter car price',
+        depositPlaceholder: 'Enter deposit amount',
+        datePlaceholder: 'Enter a delivery date'
+    },
+    yearOptions: ['1 Year', '2 Year', '3 Year']
+}
